f85c087e03c61fdc4c2d5f223f41c56541f6ef36
JellllPh <jelvinpho@gmail.com>
2024-12-10 18:47:41 +0700

    Update script.js
    sidebar

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const itemsArray = localStorage.getItem("items")
   ? JSON.parse(localStorage.getItem("items"))
   : [];
 
+ function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-250px'; // Hide sidebar
+    } else {
+        sidebar.style.left = '0px'; // Show sidebar
+    }
+}
 // Event listener for "Enter" button
 document.querySelector("#enter").addEventListener("click", () => {
   const item = document.querySelector("#item");
